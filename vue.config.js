0800b964941da98741c62a209d9d91c0cd4ffbbd
Kunju Perath <kperath@gmail.com>
2020-05-11 17:55:56 -0400

    installed tailwind successfully

---
+++ vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    devServer: {
+        // other config
+        port: 8081, // or any other port you wish to use other than 8080
+        host: "localhost" // only run from network host
+    }
+}
+
